0ce7258091ec5e283594c9374d5398ede436e7b5
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-22 00:06:01 -0300

    Design Interface Sliders

--- Organize/app/src/main/java/com/andrewmarques/android/organize/MainActivity.java
+++ Organize/app/src/main/java/com/andrewmarques/android/organize/MainActivity.java
@@ -3,12 +3,41 @@ package com.andrewmarques.android.organize;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
+
     }
 }
