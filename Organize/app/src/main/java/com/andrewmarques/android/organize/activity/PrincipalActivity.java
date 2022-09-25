f2171eae5c198dd533e3540451b5193d616c0157
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-25 09:57:09 -0300

    Floating action button menu

--- Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
+++ Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
@@ -30,12 +30,20 @@ public class PrincipalActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        binding.fabMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
     }
+
+    public void addReceita (View view){
+
+    }
+
+    public void addDespesa (View view){
+
+    }
+
 }
