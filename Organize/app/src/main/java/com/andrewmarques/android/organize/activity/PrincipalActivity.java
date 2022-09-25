2c35464300a50942292203df1843b77c6663b621
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-25 20:00:46 -0300

    tela principal

--- Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
+++ Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import com.andrewmarques.android.organize.databinding.ActivityPrincipalBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -17,10 +20,14 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.andrewmarques.android.organize.R;
 
+import java.text.SimpleDateFormat;
+
 public class PrincipalActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
+    private CalendarView calendarView;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,10 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+
+        textView = findViewById(R.id.textoTeste);
+        calendarView = findViewById(R.id.calendarView);
+        
 
         binding.fabMenu.setOnClickListener(new View.OnClickListener() {
             @Override
