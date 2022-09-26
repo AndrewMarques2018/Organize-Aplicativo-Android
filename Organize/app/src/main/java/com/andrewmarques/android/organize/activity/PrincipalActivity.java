d09ad60265d3a5d47b3a196d909ac76216ab6eff
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-25 22:56:06 -0300

    Tool bar principal

--- Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
+++ Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
@@ -5,11 +5,17 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.andrewmarques.android.organize.R;
+import com.andrewmarques.android.organize.config.ConfigFirebase;
 import com.andrewmarques.android.organize.databinding.ActivityPrincipalBinding;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.listeners.OnCalendarPageChangeListener;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -17,6 +23,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 
 public class PrincipalActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
     private CalendarView calendarView;
@@ -28,12 +35,13 @@ public class PrincipalActivity extends AppCompatActivity {
 
         binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.toolbar.setTitle("Organize");
         setSupportActionBar(binding.toolbar);
 
         txtSaudacao = findViewById(R.id.txtSaudacao);
         txtSaldo = findViewById(R.id.txtSaldo);
         calendarView = findViewById(R.id.calendarView);
-        
+
         calendarView.setPreviousButtonImage(getResources().getDrawable(R.drawable.ic_esquerda_preto));
         calendarView.setForwardButtonImage(getResources().getDrawable(R.drawable.ic_direita_preto));
 
@@ -44,6 +52,25 @@ public class PrincipalActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                auth = ConfigFirebase.getAuth();
+                auth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void addReceita (View view){
