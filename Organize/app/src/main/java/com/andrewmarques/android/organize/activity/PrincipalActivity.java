705e0599db1b8dee6d9a3f2e671a3117afa1dc49
Andrew Marques Silva <53877651+AndrewMarques2018@users.noreply.github.com>
2022-09-25 22:37:29 -0300

    tela principal

--- Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
+++ Organize/app/src/main/java/com/andrewmarques/android/organize/activity/PrincipalActivity.java
@@ -1,33 +1,26 @@
 package com.andrewmarques.android.organize.activity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import com.andrewmarques.android.organize.databinding.ActivityPrincipalBinding;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-import android.widget.CalendarView;
-import android.widget.TextView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import com.andrewmarques.android.organize.R;
+import com.andrewmarques.android.organize.databinding.ActivityPrincipalBinding;
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.listeners.OnCalendarPageChangeListener;
 
-import java.text.SimpleDateFormat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import androidx.navigation.ui.AppBarConfiguration;
+
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
     private CalendarView calendarView;
-    private TextView textView;
+    private TextView txtSaudacao, txtSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +28,15 @@ public class PrincipalActivity extends AppCompatActivity {
 
         binding = ActivityPrincipalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
 
-        textView = findViewById(R.id.textoTeste);
+        txtSaudacao = findViewById(R.id.txtSaudacao);
+        txtSaldo = findViewById(R.id.txtSaldo);
         calendarView = findViewById(R.id.calendarView);
         
+        calendarView.setPreviousButtonImage(getResources().getDrawable(R.drawable.ic_esquerda_preto));
+        calendarView.setForwardButtonImage(getResources().getDrawable(R.drawable.ic_direita_preto));
+
 
         binding.fabMenu.setOnClickListener(new View.OnClickListener() {
             @Override
